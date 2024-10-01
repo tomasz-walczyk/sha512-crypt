4057cad02dbe05886cd15aa280f9e94f5072c1af
Tomasz Walczyk <39962135+tomasz-walczyk@users.noreply.github.com>
2024-10-01 10:51:38 +0200

    Update check-release-version.bash

--- .github/workflows/scripts/check-release-version.bash
+++ .github/workflows/scripts/check-release-version.bash
@@ -105,7 +105,7 @@ fi
 # Set output variables.
 #-----------------------------------------------------------
 
-echo "::set-output name=release_name::${RELEASE_NAME}"
-echo "::set-output name=release_version::${RELEASE_VERSION}"
-echo "::set-output name=release_notes_file_name::${RELEASE_NOTES_FILE_NAME}"
-echo "::set-output name=release_notes_file_path::${RELEASE_NOTES_FILE_PATH}"
+echo "release_name=${RELEASE_NAME}" >> "${GITHUB_OUTPUT}"
+echo "release_version=${RELEASE_VERSION}" >> "${GITHUB_OUTPUT}"
+echo "release_notes_file_name=${RELEASE_NOTES_FILE_NAME}" >> "${GITHUB_OUTPUT}"
+echo "release_notes_file_path=${RELEASE_NOTES_FILE_PATH}" >> "${GITHUB_OUTPUT}"
